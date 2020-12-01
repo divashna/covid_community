a3f6351f802e1f3a677c07e67efcf3999ee2b91e
Divashna Kumar <divashna7@gmail.com>
2020-12-01 21:17:36 +1100

    Update footer

--- covid-community/general.js
+++ covid-community/general.js
@@ -1,5 +1,4 @@
 // Last Updated date in footer
-
 var lastDated = new Date(document.lastModified).toLocaleDateString();
 var dated = new Date();
 document.getElementById("footerDate").innerHTML = "Last updated " + lastDated; 
